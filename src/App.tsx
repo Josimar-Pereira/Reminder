35fd29cecb3300bc9f4059e1577a2432779d4287
Josimar-Pereira <josimabitencourt@hotmail.com>
2023-09-07 13:09:57 -0300

    React reminder

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
+import { StickyNotes } from "./components/stickyNotes";
+import NotesContext from "./hooks/notesContext";
+
+interface NotesProps {
+  title: string;
+  description: string;
+}
+
+function App() {
+  const [notes, setNotes] = useState<NotesProps[]>([]);
+  return (
+    <NotesContext.Provider value={{ notes, setNotes }}>
+      <Header />
+      <Hero />
+      <StickyNotes />
+    </NotesContext.Provider>
+  );
+}
+export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { Container } from "./styles";
+import reminderLogo from "../../assets/images/logo.jpg";
+import { Button } from "../Button";
+import { useState } from "react";
+import { NewStickyModal } from "../NewStickyModal";
+
+export function Header() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  function handleOpenModal() {
+    setIsModalOpen(true);
+  }
+  function handleCloseModal() {
+    setIsModalOpen(false);
+  }
+  return (
+    <Container>
+      <img src={reminderLogo} alt="Logo React Reminder" />
+
+      <Button title="Adicionar Lembrete" onClick={handleOpenModal} />
+      <NewStickyModal isOpen={isModalOpen} onRequestClose={handleCloseModal} />
+    </Container>
+  );
+}
